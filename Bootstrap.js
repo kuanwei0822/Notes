b3decdbc15fae8ec96bcf4b88e4dbd517b59d4f3
kuanwei0822 <as0933672360@gmail.com>
2020-12-24 22:42:51 +0800

    add bootstrap note

---
+++ Bootstrap.js
@@ -0,0 +1,6 @@
+
+外部連結:
+
+	1. CSS link:
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
